0581497cef1a9fedbe91a1a3467f1fa66106e8f9
Safaa-Ebrahim <ebrasafa@gmail.com>
2024-01-12 22:31:46 +0200

    add animate when scrolling to skill progress

--- js/main.js
+++ js/main.js
@@ -34,5 +34,17 @@ let counter = setInterval(() => {
         End Countdown Timer
                             ------------- */
 
-/* Animate Width On Scrolling */
-                                                     
+/* Animate Width On Scrolling to progress bar in  skills section */
+
+let section = document.querySelector(".our-skills");
+let progressSpans = document.querySelectorAll(".the-progress span");
+
+window.onscroll = function () {
+  if (window.scrollY >= section.offsetTop - 250) {
+    progressSpans.forEach((span) => {
+      span.style.width = span.dataset.width;
+    });
+  }
+};
+
+
