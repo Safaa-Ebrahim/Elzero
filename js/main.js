7386cf815fbc89d4274d3d276832929b3591dc5a
Safaa-Ebrahim <ebrasafa@gmail.com>
2024-01-12 22:14:35 +0200

    add countdown to event section

---
+++ js/main.js
@@ -0,0 +1,38 @@
+/* -----
+        Start Countdown Timer
+                             ------------- */
+
+// The End of the year date to Countdown To
+// 1000 ms = 1 second
+let countdownDate = new Date("Dec 31, 2024 23:59:59").getTime();
+
+let counter = setInterval(() => {
+  // Get Date Now
+  let dateNow = new Date().getTime();
+
+  // Find the Date Difference between Now and Countdown Date
+  let dateDiff = countdownDate - dateNow;
+
+  // Get Time Units
+  let days = Math.floor(dateDiff / (1000 * 60 * 60 * 24));
+  let hours = Math.floor((dateDiff % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+  let minutes = Math.floor((dateDiff % (1000 * 60 * 60)) / (1000 * 60));
+  let seconds = Math.floor((dateDiff % (1000 * 60)) / 1000);
+
+  document.querySelector(".days").innerHTML = days < 10 ? `0${days}` : days;
+  document.querySelector(".hours").innerHTML = hours < 10 ? `0${hours}` : hours;
+  document.querySelector(".minutes").innerHTML =
+    minutes < 10 ? `0${minutes}` : minutes;
+  document.querySelector(".seconds").innerHTML =
+    seconds < 10 ? `0${seconds}` : seconds;
+
+  if (dateDiff < 0) {
+    clearInterval(counter);
+  }
+}, 1000);
+/* -----
+        End Countdown Timer
+                            ------------- */
+
+/* Animate Width On Scrolling */
+                                                     
